2f6388627bdb166993ea6b6860f8664207a0f86e
Kie0530 <kai6666@naver.com>
2022-06-22 23:12:44 +0900

    Leetcode 17 letter combinations of phone keypad number using backtracking algorithm. Still haven't solved the problem with case 'when digits include numbers zero and one.

--- IdeaProjects/Practice/src/Main/Java/DataStructureandAlgorithm/LetterCombinationsPhoneNum.java
+++ IdeaProjects/Practice/src/Main/Java/DataStructureandAlgorithm/LetterCombinationsPhoneNum.java
@@ -1,11 +1,11 @@
 package Main.Java.DataStructureandAlgorithm;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.sql.Array;
+import java.util.*;
 
 
-    public class LetterCombinationsPhoneNum  {
+public class LetterCombinationsPhoneNum  {
+
         public static void main(String[] args)  throws Exception {
 
             System.out.println(letterCombinations("23"));
@@ -15,36 +15,61 @@ import java.util.List;
             System.out.println(letterCombinations(""));
             //[]
             System.out.println(letterCombinations("50"));
-            //[[j , k , l ]]
+            //[]
             System.out.println(letterCombinations("11"));
-            //[  ]
+            //[]
 
 
         }
+
         public static List<String> letterCombinations(String digits) {
-            LinkedList<String> answer = new LinkedList<>();
-            answer.add(""); // 널포인터익셉션 해결
+            List<String> ans = new ArrayList<>();
+            if(digits.length()==0) return ans;
 
-            // digits가 빈 경우
-            if(digits.length()==0) return answer;
+            backtrack(digits, 0, new StringBuilder(), ans);
+            return ans;
+        }
 
-            // 키패드 번호가 알파벳 있는 인덱스와 일치하게 키패드 알파벳 배열 생성
-            String[] num = {" ", " ", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-            // 반복문으로 answer 채우기
-            // 주어지는 digits의 두 수를 for문이 확인할 것
-            for(int i = 0; i < digits.length(); i++) {
-                // 인덱스 만들어주기
-                int j = digits.charAt(i) -'0';
+        private static void backtrack(String digits, int start, StringBuilder sb, List<String> ans) {
+            int len = sb.length();
+            if(len == digits.length()) {
+                ans.add(sb.toString());
+                return;
+            }
 
-                // 아래 while문 안에 add 메서드를 넣자 널포인터익셉션 에러가 났다. (위로 옮겨서 해결)
-                while(answer.peek().length()==i) {
-                    String s = answer.remove();
-                    for(char c : num[j].toCharArray())
-                        answer.add(s+c);
+            for(int i = start; i < digits.length(); i++) {
+                String letters = getLetters(digits.charAt(i));
+
+                for(int j = 0; j < letters.length(); j++) {
+                    sb.append(letters.charAt(j));
+                    backtrack(digits, i + 1,sb, ans);
+                    sb.setLength(len);
                 }
-            } return answer;
+            }
+        }
 
+
+        private static String getLetters(char c) {
+            switch(c) {
+                case '2' :
+                    return "abc";
+                case '3' :
+                    return "def";
+                case '4' :
+                    return "ghi";
+                case '5' :
+                    return "jkl";
+                case '6' :
+                    return "mno";
+                case '7' :
+                    return "pqrs";
+                case '8' :
+                    return "tuv";
+                case '9' :
+                    return "wxyz";
+            }
+            return "";
         }
     }
 
